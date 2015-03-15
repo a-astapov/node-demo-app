75007ebe80c82b0f74bf395b8685f090e77825bd
a-astapov <astapov.artem@physics.msu.ru>
2015-03-15 20:30:32 +0400

    Added home page

---
+++ app.js
@@ -0,0 +1,1 @@
+﻿module.exports = require(__dirname + '/app/main')
---
+++ app/main.js
@@ -0,0 +1,14 @@
+﻿var express = require('express')
+  , app = express()
+  , pages = require(__dirname + '/controllers/pages') 
+
+// configuration settings 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+// mount routes
+app.get('/', function (req, res) { res.redirect('home') })
+app.get('/home', pages.home)
+app.get('/about', pages.about)
+module.exports = app
